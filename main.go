9378d2d92860b1d6b0ee03695f37a10a65340cf4
shlu <shilulu@forensix.cn>
2023-09-11 11:16:00 +0800

    [apk] 支持备份APK

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ type DataManager struct {
 	BackupFilePath string
 	BusyboxPath    string
 	AdbPath        string
+	IsForceStop    bool
+	isSkipApk      bool
 }
 
 func NewDataManager(adbPath string) *DataManager {
@@ -24,22 +26,48 @@ func NewDataManager(adbPath string) *DataManager {
 	ptr.BackupFilePath = "/data/local/tmp"
 	ptr.BusyboxPath = ""
 	ptr.AdbPath = adbPath
+	ptr.IsForceStop = true
+	ptr.isSkipApk = true
 	return ptr
 }
+func (that *DataManager) SetForceStop(forceStop bool) {
+	that.IsForceStop = forceStop
+}
+func (that *DataManager) SetSkipApk(skipApk bool) {
+	that.isSkipApk = skipApk
+}
+
 func (that *DataManager) Backup(packageName string) error {
 	savePath := fmt.Sprintf("%s/HBackup_%s_%s_%s_%s.zip",
 		that.fixPath(that.BackupFilePath),
 		packageName,
 		that.fixPath(that.getVersionName(packageName)), that.fixPath(that.getModel()), time.Now().Format("20060102_150405"))
-	commands := []string{
-		fmt.Sprintf("am force-stop %s", packageName),
+	commands := make([]string, 0)
+	if that.IsForceStop {
+		commands = append(commands, fmt.Sprintf("am force-stop %s", packageName))
+	}
+	apkPath := "\"\""
+	if that.isSkipApk {
+		errPtr := new(error)
+		apkPath = dcmd.Exec_(that.AdbPath+" shell pm path "+packageName, errPtr)
+		if *errPtr != nil {
+			return *errPtr
+		}
+		apkPath = strings.TrimSpace(apkPath)
+		apkPath = strings.Split(apkPath, ":")[1]
+		apkPath = fmt.Sprintf("\"%s\"", apkPath)
+		log.Println("apk path:", apkPath)
+	}
+	commands = append(commands,
 		fmt.Sprintf("%s rm -rf /data/data/.external.%s", that.BusyboxPath, packageName),
 		fmt.Sprintf("%s ln -sf \"/sdcard/Android/data/%s\" \"/data/data/.external.%s\"", that.BusyboxPath, packageName, packageName),
-		fmt.Sprintf("%s tar -c \"/data/data/%s/\" \"/data/data/.external.%s/.\" --exclude \"data/%s/lib/\" > \"%s\"", that.BusyboxPath, packageName, packageName, that.BusyboxPath, savePath),
-		fmt.Sprintf("%s rm -rf \"/data/data/.external.%s\"", that.BusyboxPath, packageName),
-		fmt.Sprintf("am force-stop %s", packageName),
-		fmt.Sprintf("%s chown media_rw:media_rw \"%s\"", that.BusyboxPath, savePath),
+		fmt.Sprintf("%s tar -c \"/data/data/%s/\" \"/data/data/.external.%s/.\" %s --exclude \"data/data/%s/lib/\" > \"%s\"", that.BusyboxPath, packageName, packageName, apkPath, packageName, savePath),
+		fmt.Sprintf("%s rm -rf \"/data/data/.external.%s\"", that.BusyboxPath, packageName))
+	if that.IsForceStop {
+		commands = append(commands, fmt.Sprintf("am force-stop %s", packageName))
 	}
+	commands = append(commands, fmt.Sprintf("%s chown media_rw:media_rw \"%s\"", that.BusyboxPath, savePath))
+
 	err := that.ExecCommands(commands...)
 	if err != nil {
 		return err
@@ -139,11 +167,15 @@ func main() {
 	adbPath := "adb"
 	var packageName string
 	var serialNumber string
+	var isForceStop bool
+	var isSkipApk bool
 	flag.StringVar(&adbPath, "a", "adb", "adb path")
 	flag.StringVar(&serialNumber, "s", "", "device serial number")
+	flag.BoolVar(&isForceStop, "f", true, "force stop target app")
+	flag.BoolVar(&isSkipApk, "k", true, "skip apk file")
 	flag.Parse()
 	if flag.NArg() != 1 || flag.Arg(0) == "" {
-		println("The packageName must be specified.\neg:\nhbackup xxx.xxx.xxx")
+		println("The packageName must be specified.\neg:\nhbackup [-a=value] [-s=value] [-k=true/false] [-f=true/false] xxx.xxx.xxx")
 		return
 	}
 	packageName = flag.Arg(0)
@@ -151,7 +183,10 @@ func main() {
 	if serialNumber != "" {
 		adbPath += " -s " + serialNumber
 	}
-	err := NewDataManager(adbPath).Backup(packageName)
+	dataManager := NewDataManager(adbPath)
+	dataManager.SetForceStop(isForceStop)
+	dataManager.SetSkipApk(isSkipApk)
+	err := dataManager.Backup(packageName)
 	if err != nil {
 		println("HBackup error:", err)
 	} else {
